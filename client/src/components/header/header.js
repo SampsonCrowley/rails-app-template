0397bffaeb7c65fbcf8695b18fdc2165deda2bbf
Sampson Crowley <sampsonsprojects@gmail.com>
2018-05-21 23:06:21 -0600

    more style touches on client

--- client/src/components/header/header.js
+++ client/src/components/header/header.js
@@ -7,16 +7,16 @@ export default class Header extends Component {
     return (
       <header className="Site-header">
         <div className="header-content">
-          <nav class="header-nav">
-            <div class="header-logo">
-              <a href="/" class="header-link">
-                <img src={logo} class="header_link__2zJJT header_white__3qPyi" />
+          <nav className="header-nav">
+            <div className="header-logo">
+              <a href="/" className="header-link">
+                <img src={logo} alt='Site Logo' />
               </a>
             </div>
-            <div class="header-menu">
+            <div className="header-menu">
               <div className="header-menu-wrapper">
-                <a href="#/" class="header-link">Home Page</a>
-                <a href="#/page" class="header-link">Test Page</a>
+                <a href="#/" className="header-link">Home Page</a>
+                <a href="#/page" className="header-link">Test Page</a>
               </div>
             </div>
           </nav>
