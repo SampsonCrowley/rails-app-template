92fc5c12fd9075c67d49af4cf70a7d3895d917ed
Sampson Crowley <sampsonsprojects@gmail.com>
2018-07-20 16:15:57 -0600

    update application record methods

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,6 +1,43 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
+  # == Constants ============================================================
+
+  # == Attributes ===========================================================
+
+  # == Extensions ===========================================================
+
+  # == Relationships ========================================================
+
+  # == Validations ==========================================================
+  before_validation :set_booleans
+
+  # == Scopes ===============================================================
+
+  # == Callbacks ============================================================
+
+  # == Class Methods ========================================================
+  def self.boolean_columns
+    []
+  end
+
+  def self.default_print
+    column_names
+  end
+
+  def self.find_or_retry_by(*args)
+    found = nil
+    retries = 0
+    begin
+      raise ActiveRecord::RecordNotFound unless found = find_by(*args)
+      return found
+    rescue
+      sleep retries += 1
+      retry if (retries) < 5
+    end
+    found
+  end
+
   def self.queue_adapter_inline?
     @@queue_adapter ||= Rails.application.config.active_job.queue_adapter
     @@queue_adapter == :inline
@@ -36,6 +73,7 @@ class ApplicationRecord < ActiveRecord::Base
     end
   end
 
+  # == Instance Methods =====================================================
   def queue_adapter_inline?
     self.class.queue_adapter_inline?
   end
@@ -49,5 +87,17 @@ class ApplicationRecord < ActiveRecord::Base
   def purge(attached)
     attached.__send__ queue_adapter_inline? ? :purge : :purge_later
   end
+
+  def boolean_columns
+    self.class.boolean_columns
+  end
+
+  private
+    def set_booleans
+      self.class.boolean_columns.each do |nm|
+        self.__send__("#{nm}=", __send__("#{nm}=", !!Boolean.parse(__send__ nm)))
+      end
+      true
+    end
 
 end
